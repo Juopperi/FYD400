51e6d63b3bad446773a91e098890bb7f68d6e8d5
Juopperi <albin.juopperi@gmail.com>
2023-09-14 22:32:35 +0200

    update

--- chapter 1/157.c
+++ chapter 1/157.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(){
     int input = 0,index =0;
@@ -8,20 +9,24 @@ int main(){
     printf("Hur m\x84nga ord vill du mata in) ");
     scanf("%d",&input);
 
-    char *ord = malloc(sizeof(char)*20);
+    char *ord = malloc(sizeof(char)*100);
 
     printf("Ge %d ord nu:\n",input);
-    
+    rewind(stdin);
+
     while(1){
         scanf("%c",&inputOrd);
-        if(inputOrd == 'o'){
+        if(inputOrd == '\n'){
             break;
         }
         ord[index]=inputOrd;
         index++;
     }
 
-    for(int i = 0; i<sizeof(*ord); i++){
+    ord[index] = '\0';
+    printf("H\x84r \x84r dina ord:\n");
+
+    for(int i = 0; i<strlen(ord); i++){
         if(ord[i] != 32){
             printf("%c",*(ord+i));
         } else {
