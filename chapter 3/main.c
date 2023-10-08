4494b06914e7f47d5807c1857e5240c33400ee36
Juopperi <albin.juopperi@gmail.com>
2023-10-08 21:45:55 +0200

    trying to make gui work

---
+++ chapter 3/main.c
@@ -0,0 +1,20 @@
+#include <gtk/gtk.h>
+
+int main(int argc, char *argv[]) {
+    gtk_init(&argc, &argv);
+
+    GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_title(GTK_WINDOW(window), "GTK Hello World");
+    gtk_window_set_default_size(GTK_WINDOW(window), 300, 200);
+    gtk_container_set_border_width(GTK_CONTAINER(window), 10);
+
+    GtkWidget *label = gtk_label_new("Hello, GTK!");
+    gtk_container_add(GTK_CONTAINER(window), label);
+
+    g_signal_connect(G_OBJECT(window), "destroy", G_CALLBACK(gtk_main_quit), NULL);
+
+    gtk_widget_show_all(window);
+    gtk_main();
+
+    return 0;
+}
